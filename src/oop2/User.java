7d30e885b51418c8c1ae0316700302486ad583ce
kota <gnsukumsukuymmtu@gmail.com>
2023-04-25 23:38:23 +0900

    演習オーバーライド（未完）

---
+++ src/oop2/User.java
@@ -0,0 +1,17 @@
+package oop2;
+
+public class User {
+    String name;
+    String from;
+    int age;
+
+    @Override
+    public boolean equals(Object obj){
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
---
+++ src/oop2/main.java
@@ -0,0 +1,8 @@
+package oop2;
+
+public class main {
+    public static void main(String[] args) {
+        var i = new User();
+        System.out.println(i);
+    }
+}
