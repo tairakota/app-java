d3a561223f71f3832d0bc8285432eba75a5368e1
taira kota <taira.kota.2023.kensyu@gmail.com>
2023-04-26 17:13:06 +0900

    例外-3（未完）

--- src/exception/ExceptionPractice.java
+++ src/exception/ExceptionPractice.java
@@ -1,17 +1,15 @@
 package exception;
-import java.lang.Throwable;
 
 public class ExceptionPractice {
 
     public static void main(String[] args) {
-        var i = judgeScore(101);
-        System.out.println(i);
+        System.out.println(ScannerNum.scannerNum());
     }
 
     public static String judgeScore(int score) {
         if (score > 100 || score < 0) {
-            IlligalScoreExceptionTest();
-            return "";
+            IlligalScoreException.erra();
+            return ScannerNum.scannerNum();
         } else if (score >= 80 && score <= 100) {
             return "A";
         } else if (score >= 70) {
@@ -23,13 +21,8 @@ public class ExceptionPractice {
         }
     }
 
-    public static void IlligalScoreExceptionTest() {
-        throw new IlligalScoreException();
-    }
+//    public static void IlligalScoreExceptionTest() {
+//        throw new IlligalScoreException();
+//    }
 }
 
-//scoreの値が80以上の場合は"A"を返す
-//        scoreの値が70以上の場合は"B"を返す
-//        scoreの値が60以上の場合は"C"を返す
-//        scoreの値が59以下の場合は"D"を返す
-//        scoreの値が負の値、または100より大きい場合はIlligalScoreExceptionの例外を投げる
--- src/exception/IlligalScoreException.java
+++ src/exception/IlligalScoreException.java
@@ -2,4 +2,13 @@ package exception;
 
 public class IlligalScoreException extends RuntimeException{
 
+    public static void erra() {
+        try {
+            throw new IlligalScoreException();
+        }catch (IlligalScoreException e){
+            System.out.println("test");
+            ScannerNum.scannerNum();
+        }
+
+    }
 }
