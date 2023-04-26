ed9fbe786909f5b276e6182db924d175c8efbb2d
taira kota <taira.kota.2023.kensyu@gmail.com>
2023-04-26 14:06:16 +0900

    演習オーバーライド

--- src/oop2/User.java
+++ src/oop2/User.java
@@ -1,17 +1,29 @@
 package oop2;
 
+import java.util.Objects;
+
 public class User {
     String name;
     String from;
-    int age;
+    int birth;
+
+    public User(String name, String from, int birth) {
+        this.name = name;
+        this.from = from;
+        this.birth = birth;
+    }
 
     @Override
     public boolean equals(Object obj){
-        return true;
+        User i = (User)obj;
+        if (this.name == i.name && this.from == i.from && this.birth == i.birth) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public String toString() {
-        return "";
+        return "名前：" + name + ",　出身地：" + from + ",　生年月日：" + birth;
     }
 }
--- src/oop2/main.java
+++ src/oop2/main.java
@@ -2,7 +2,9 @@ package oop2;
 
 public class main {
     public static void main(String[] args) {
-        var i = new User();
-        System.out.println(i);
+        var i = new User("cent", "tokoy", 20040613);
+        var j = new User("cent", "tokoy", 20040613);
+        System.out.println(i.equals(j));
+        System.out.println(i.toString());
     }
 }
